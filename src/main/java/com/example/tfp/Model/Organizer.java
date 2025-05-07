0a429f7a34a774f260039a58aa3020607ef5d155
Faisal <faisal.a.m.2012@gmail.com>
2025-05-08 00:09:41 +0300

    Models

--- src/main/java/com/example/tfp/Model/Organizer.java
+++ src/main/java/com/example/tfp/Model/Organizer.java
@@ -1,4 +1,37 @@
 package com.example.tfp.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Organizer {
+
+    @Id
+    private Integer id;
+
+    @NotEmpty(message = "License number must not be empty")
+    @Column(columnDefinition = "varchar(10) not null unique")
+    private String licenceNumber;
+
+    @NotEmpty(message = "Status must not be empty")
+    @Column(columnDefinition = "varchar(20) not null")
+    private String status;
+
+    @OneToOne
+    @MapsId
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Field> fields;
+
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<PublicMatch> publicMatches;
 }
